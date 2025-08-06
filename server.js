9ad13a2e7d49291d2c3172799feed795395b16b6
unknown <sarthakagrwal0@gmail.com>
2025-08-06 18:34:37 +0530

    fixed desktop preview issue

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ const server = http.createServer((req, res) => {
 });
 
 // Serve files with caching and headers
-function serveFile(res, filePath, contentType) {
+/*function serveFile(res, filePath, contentType) {
   fs.readFile(filePath, (err, content) => {
     if (err) {
       res.writeHead(500, { "Content-Type": "text/plain" });
@@ -108,7 +108,23 @@ function serveFile(res, filePath, contentType) {
       res.end(content);
     }
   });
+}*/
+function serveFile(res, filePath, contentType) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Error loading file");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": contentType,
+        // ✅ Add this header to allow PDF embedding
+        "X-Frame-Options": "ALLOWALL"
+      });
+      res.end(data);
+    }
+  });
 }
+
 
 server.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
